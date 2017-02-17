22c9ca70fb5b9c30b3dda6b0314cc83d83338971
renatoms <renatoms@TRDSP-10342.triad.sp>
2017-02-17 10:24:03 -0200

    Aula 5.

--- Java8/src/br/com/java8/testes/ProcessaCursos.java
+++ Java8/src/br/com/java8/testes/ProcessaCursos.java
@@ -3,7 +3,12 @@ package br.com.java8.testes;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.function.Consumer;
+import java.util.function.DoubleConsumer;
 import java.util.function.Predicate;
+import java.util.function.ToIntFunction;
+import java.util.stream.Collectors;
 
 public class ProcessaCursos {
 	
@@ -27,6 +32,32 @@ public class ProcessaCursos {
 		System.out.println("------------");
 		
 		cursos.forEach((Curso c) -> System.out.println(c.getNome()));
+		
+		// Pegando o primeiro elemento da Stream:
+		
+		System.out.println("Primeiro elemento da stream:");
+		
+		cursos.stream()
+			.filter(c -> c.getAlunos() >= 50)
+			.findFirst().ifPresent( t -> System.out.println(t.getNome()));
+		
+		List<Curso> collect = cursos.stream()
+			.filter(c -> c.getAlunos() >= 50)
+			.collect(Collectors.toList());
+		
+		System.out.println("Lista de cursos filtrados: " + collect);
+		
+		// Calculando a média de alunos em todos os cursos:
+		
+		System.out.println("Média de alunos por curso:");
+		
+		//Double media = cursos.stream().collect(Collectors.averagingInt(Curso::getAlunos));
+		//System.out.println("Média de alunos: " + media);
+		
+		cursos.stream()
+	    .mapToInt(c -> c.getAlunos())
+	    .average().ifPresent(d -> System.out.println(d));
+		
 		
 		
 	}
